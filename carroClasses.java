070aa740de41fd424cdca15ae4d23601d98690fb
  <kevindasilvalima2008@gmail.com>
2026-08-25 16:01:17 -0300

    Classes com metodos

--- carroClasses.java
+++ carroClasses.java
@@ -1,5 +1,90 @@
+import java.util.Scanner;
+
 public class carroClasses {
-    static class Carro{
-        String 
+    static Scanner sc = new Scanner(System.in);
+
+    static class Carro{//Sem construtor
+        String marca;
+        String modelo;
+        int velocidade;
+
+        void acelerar(){
+            velocidade += 10;
+        }
+    }
+
+    public static void exemploUm(){
+        Carro carro1 = new Carro();
+
+        String c_marca = carro1.marca;
+        String c_modelo = carro1.modelo;
+        carro1.acelerar();
+        carro1.acelerar();
+        
+        System.out.println("Digite a marca: ");
+        c_marca = sc.nextLine();
+
+        System.out.println("Digite o modelo");
+        c_modelo = sc.nextLine();
+
+        System.out.println("---------------------------");
+
+        System.out.println("Marca do carro: " + c_marca);
+        System.out.println("Modelo: " + c_modelo);
+        System.out.println("Velocidade: " + carro1.velocidade);
+        
+    }
+
+    static class Carro2 {
+        String marca;
+        String modelo;
+        int velocidade;
+
+        int acelerar(int vel){
+            velocidade += vel;
+            return velocidade;
+        }
+
+    }
+
+
+    public static void exemploDois(){
+        Carro2 carro = new Carro2();
+
+        String marcaCarro = carro.marca;
+        String modeloCarro = carro.modelo;
+        int velocidadeCarro = carro.velocidade;
+        
+
+        System.out.println("Digite a marca do carro");
+        marcaCarro = sc.nextLine();
+
+        System.out.println("Digite a modelo do carro");
+        modeloCarro = sc.nextLine();
+
+        System.out.println("Digite a velocidade do carro");
+        velocidadeCarro = sc.nextInt();
+
+
+        System.out.println("---------------------------");
+
+        System.out.println("Marca do carro: " + marcaCarro);
+        System.out.println("Modelo: " + modeloCarro);
+        System.out.println("Velocidade: " + velocidadeCarro);
+
+        System.out.println("----------------------------");
+
+        velocidadeCarro += carro.acelerar(20);
+        System.out.println("O carro acelerou 20");
+        System.out.println("Velocidade final: " + velocidadeCarro);
+        
+
+    }
+
+
+
+    public static void main(String[] args) {
+        //exemploUm();
+        exemploDois();
     }
 }
